4a26cac56f7861fccfd2f7279a46014d15b3ff64
Alex Ahumada <projectsperminute@gmail.com>
2026-04-21 08:35:24 +0200

    feat: add engine docker files and package directories

---
+++ engine/vinculante/domain/entities/__init__.py
@@ -0,0 +1,6 @@
+from .proposal import Proposal
+from .target_document import TargetDocument
+from .section import Section
+from .match import Match, MatchStatus
+
+__all__ = ["Proposal", "TargetDocument", "Section", "Match", "MatchStatus"]
